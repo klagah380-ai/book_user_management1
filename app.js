96fc054be64132347d9ce9a35a152f59eab9c56c
klagah380-ai <klagah380@gmail.com>
2026-02-11 14:50:17 +0530

    Updated for render deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.Port ||3000;
 
 const bookRoutes = require('./routes/books');
 const userRoutes = require('./routes/users');
